38efa05a9d133924ee4acd094c6400a68e0f79af
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2024-02-03 22:35:39 +0330

    first version

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Redux Toolkit Counter</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
